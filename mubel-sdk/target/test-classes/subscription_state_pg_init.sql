1712bef868da77335b8e2ad60fc9c0d2fc3aa953
helun <henrikbd@hey.com>
2023-11-09 14:08:05 +0100

    remove checked in stuff in target dirs

--- mubel-sdk/target/test-classes/subscription_state_pg_init.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS subscription_state (
-    consumer_group VARCHAR(255) NOT NULL,
-    sequence_no BIGINT NOT NULL,
-    version INT NOT NULL,
-    PRIMARY KEY (consumer_group)
-    );
